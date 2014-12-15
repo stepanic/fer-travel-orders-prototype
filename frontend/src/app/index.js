fa3fb2258782d9554cfe3d86ec6d68b126499749
Matija Stepanic <stepanic.matija@gmail.com>
2014-12-15 16:40:09 +0100

    Edited Frontend homepage, created App Layout

--- frontend/src/app/index.js
+++ frontend/src/app/index.js
@@ -1,14 +1,25 @@
 'use strict';
 
 angular.module('frontend', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ui.router'])
-  .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      });
+  .config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    '$locationProvider',
+    function ($stateProvider, $urlRouterProvider, $locationProvider) {
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: 'app/main/main.html',
+          controller: 'MainCtrl'
+        });
 
-    $urlRouterProvider.otherwise('/');
-  })
+      $urlRouterProvider.otherwise('/');
+
+      // Yeah we wanna to use HTML5 urls!
+      $locationProvider
+          .html5Mode(true)
+          .hashPrefix('!')
+      ;
+    }
+  ])
 ;
